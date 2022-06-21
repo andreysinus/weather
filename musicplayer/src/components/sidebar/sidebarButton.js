33ab6241e593afaf88a462a243c440f9c5fbff85
andreysinus <79992614510@ya.ru>
2022-06-21 13:27:22 +0300

    commit

--- musicplayer/src/components/sidebar/sidebarButton.js
+++ musicplayer/src/components/sidebar/sidebarButton.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import "./sidebarButton.css"
+import React from 'react';
+import { IconContext } from 'react-icons/lib';
+import { Link, useLocation } from "react-router-dom";
+import './sidebarButton.css';
 
 
+export default function SidebarButton(props) {
 
-export default function sidebarButton() {
+  const isActive= useLocation().pathname === props.to;
+
+  const btnClass=  isActive ? "btn-body active" : "btn-body";
+
   return (
-    <div>B</div>
-  )
+    <Link to = {props.to}>
+      <div className={btnClass}>
+        <IconContext.Provider value={{size:"24px", className: "btn-icon"}}>
+          {props.icon}
+          <p className='btn-title'> {props.title}</p>
+        </IconContext.Provider>
+      </div>
+    </Link>
+  );
 }
