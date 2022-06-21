6f68bc409d39fcbca55dc4c2b2ec9ddb525c0905
andreysinus <79992614510@ya.ru>
2022-06-21 13:27:15 +0300

    commit

--- musicplayer/src/components/sidebar/index.js
+++ musicplayer/src/components/sidebar/index.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import './sidebar.css'
 import myLogo from './logo.svg'
-import "./sidebarButton.js"
+import SidebarButton from './sidebarButton';
+import { MdFavorite } from "react-icons/md";
+import { MdCalendarViewDay } from "react-icons/md";
+import { MdLibraryBooks } from "react-icons/md";
+import { MdLocalFireDepartment } from "react-icons/md";
+import { FaSignOutAlt, FaPlay } from "react-icons/fa";
 
 export default function Sidebar() {
   return (
     <div className='sidebar-container'>
         <img className='logo-img' src={myLogo} alt='Player'/>
         <div>
-            <sidebarButton />
-            <sidebarButton />
-            <sidebarButton />
-            <sidebarButton />
+            <SidebarButton title="Feed" to="/feed" icon={<MdCalendarViewDay/>}/>
+            <SidebarButton title="Trending" to="/trending" icon={<MdLocalFireDepartment/>}/>
+            <SidebarButton title="Player" to="/player" icon={<FaPlay/>}/>
+            <SidebarButton title="Favourite" to="/favourites" icon={<MdFavorite/>}/>
+            <SidebarButton title="Library" to="/" icon={<MdLibraryBooks/>}/>
         </div>
+        <SidebarButton title="Sign out" to="" icon= {<FaSignOutAlt/>}/>
     </div>
   ) 
 }
