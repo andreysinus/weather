5bba7b22f199fcc8054d5a3e5c9b6b9d6714d0f7
andreysinus <79992614510@ya.ru>
2022-06-22 10:22:32 +0300

    Commit

---
+++ musicplayer/src/screens/settings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Settings() {
+  return (
+    <div className='screen-container'>
+      <a className='Head-text'>SETTINGS</a>
+      <div>
+        <a></a>
+      </div>
+    </div>
+  )
+}
